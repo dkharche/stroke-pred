faca34d8e3ff544b481fa41fc3b89e1e5fd2ac0e
DIVIJ KHARCHE <74908218+dkharche@users.noreply.github.com>
2023-04-03 20:54:11 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,66 +1,71 @@
 #importing the necessary libraries
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
+import bcrypt
 
-#configuring the application
 
-app = Flask(__name__ ,template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-#Initializing the database
-db = SQLAlchemy(app)
+app = Flask(__name__, template_folder='template')
+client = MongoClient("mongodb+srv://divijkharche01:LEATHERbat01@cluster0.g4wwula.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database('db')
 
-#Creating the database table (user)
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    EmailID = db.Column(db.String(80))
-    password = db.Column(db.String(80))
-
-#Creating the home page
 @app.route('/')
 def home():
     return render_template('home.html')
-    
 
-#Creating the login page
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/register.html', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        users = db.user
+        existing_user = users.find_one({'username': request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'username': request.form['username'], 'password': hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+@app.route('/login.html', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        EmailID = request.form['EmailID']
-        password = request.form['password']
-        user = User.query.filter_by(EmailID=EmailID, password=password).first()
-        if user:
-            return redirect(url_for('success'))
-        else:
-            return render_template('login.html', error='Invalid username or password')
-    else:
-        return render_template('login.html')
+        users = db.user_data
+        login_user = users.find_one({'username': request.form['username']})
 
-#Creating the register page
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        EmailID = request.form['EmailID']
-        password = request.form['password']
-        user = User(EmailID=EmailID, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    else:
-        return render_template('register.html')
+        if login_user:
+            if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_user['password']):
+                session['username'] = request.form['username']
+                return redirect(url_for('home'))
+            else:
+                return 'Invalid username/password combination'
 
-#Creating the success page
+        return 'Invalid username/password combination'
+
+    return render_template('login.html')
+
 @app.route('/success')
 def success():
-    return render_template('success.html')
+    if 'username' in session:
+        return render_template('stroke.html', username=session['username'])
 
-#Creating the Google sign in page
-@app.route('/google_login')
-def google_login():
-    return render_template('google_login.html')
+    return redirect(url_for('login'))
 
-#Running the application
+# @app.route('/logout')
+# def logout():
+#     session.clear()
+#     return redirect(url_for('login'))
+#
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+
+# #Running the application
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
